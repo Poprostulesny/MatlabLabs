aabb5dec44a224db428fa24083259591d1da3842
Mateusz Marek Leśniczak <82328674+Poprostulesny@users.noreply.github.com>
2026-04-28 13:38:04 +0200

    update definitions

--- Iterate.m
+++ Iterate.m
@@ -1,10 +1,10 @@
-function x = Iterate(A, b, x)
+function x_new = Iterate(A, b, x)
 % Computes one iteration of the Jacobi method for a Hessenberg matrix
 % Input:
 % A - Hessenberg Matrix
 % b - answer vector
 % x - current solution vector
 % Return:
-% x - updated solution array
+% x_new - updated solution array
 
 end
